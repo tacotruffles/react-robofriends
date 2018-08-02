2ad065deafe13c8a1290b3427fdeda32fc8ff17d
John Dawes <jdawes@bluebitmedia.com>
2018-08-02 16:10:30 -0600

    Folder cleanup

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import CardList from "./components/CardList";
-import SearchBox from "./components/SearchBox";
-import Scroll from "./components/Scroll";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../hoc/Scroll";
 
 // CSS
 import "./App.css";
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "tachyons";
 import "./index.css";
 
 // Parent App
-import App from "./App";
+import App from "./containers/App";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
