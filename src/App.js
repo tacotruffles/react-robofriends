fdbe5542ef9de5d17e8fbbffde947f78136875d9
John Dawes <jdawes@bluebitmedia.com>
2018-08-02 16:05:07 -0600

    Scroll HOC

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 import CardList from "./components/CardList";
 import SearchBox from "./components/SearchBox";
+import Scroll from "./components/Scroll";
 
 // CSS
 import "./App.css";
 
-// Data
-import { robots } from "./data/robots";
-
 class App extends Component {
   state = {
-    robots: robots,
+    robots: [],
     filter: ""
   };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }))
+      .catch(error => console.log(error));
+  }
 
   onChangeHandler = event => {
     // Set the Filter in State
@@ -27,13 +32,19 @@ class App extends Component {
         .includes(this.state.filter.toLowerCase());
     });
 
-    return (
-      <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox filter={this.onChangeHandler} value={this.state.filter} />
-        <CardList robots={filteredSearch} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1 className="tc">Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1>RoboFriends</h1>
+          <SearchBox filter={this.onChangeHandler} value={this.state.filter} />
+          <Scroll>
+            <CardList robots={filteredSearch} />
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
 
