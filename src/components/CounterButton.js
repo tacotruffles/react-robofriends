5d406bb0c860f216787181f12c98362c78976110
John Dawes <jdawes@bluebitmedia.com>
2018-08-04 15:58:18 -0600

    Gitpage update

---
+++ src/components/CounterButton.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class CounterButton extends Component {
+  state = {
+    count: 0
+  };
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.count != nextState.count) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  onUpdateCount = () => {
+    this.setState(state => ({ count: this.state.count + 1 }));
+  };
+  render() {
+    return (
+      <button color={this.props.color} onClick={this.onUpdateCount}>
+        Count: {this.state.count}
+      </button>
+    );
+  }
+}
+
+export default CounterButton;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import CounterButton from './CounterButton';
+
+class Header extends Component {
+  // Just render this component once!
+  shouldComponentUpdate(nextProps, nextState) {
+    return false;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>RoboFriendz</h1>
+        <CounterButton color={'red'} />
+      </div>
+    );
+  }
+}
+
+export default Header;
