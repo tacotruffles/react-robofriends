311a94d0fcf446b634010a778b9a1ad73a93b00b
John Dawes <jdawes@bluebitmedia.com>
2018-08-04 15:57:38 -0600

    Render optimization

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
-import CardList from "../components/CardList";
-import SearchBox from "../components/SearchBox";
-import Scroll from "../hoc/Scroll";
-import ErrorBoundary from "../components/ErrorBoundary";
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../hoc/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 
-import { connect } from "react-redux";
-import * as actions from "../store/actions";
+import { connect } from 'react-redux';
+import * as actions from '../store/actions';
 
 // CSS
-import "./App.css";
+import './App.css';
 
 class App extends Component {
   componentDidMount() {
@@ -26,7 +27,7 @@ class App extends Component {
       <h1 className="tc">Loading</h1>
     ) : (
       <div className="tc">
-        <h1>RoboFriendz</h1>
+        <Header />
         <SearchBox filter={onSearchChange} value={filter} />
         <Scroll>
           <ErrorBoundary>
--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore, applyMiddleware, combineReducers } from "redux";
-import { createLogger } from "redux-logger";
-import thunkMiddleware from "redux-thunk";
-import { Provider } from "react-redux";
-import { searchReducer, robotReducer } from "./store/reducers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+//import { createLogger } from "redux-logger";
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { searchReducer, robotReducer } from './store/reducers';
 
 // CSS
-import "tachyons";
-import "./index.css";
+import 'tachyons';
+import './index.css';
 
 // Parent App
-import App from "./containers/App";
-import registerServiceWorker from "./registerServiceWorker";
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
 
 // Redux Middleware
-const logger = createLogger();
+//const logger = createLogger();
 
 // Redux Stores
 const rootReducer = combineReducers({
   searchReducer,
   robotReducer
 });
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware)); //, logger
 
 // App
 const app = (
@@ -31,5 +31,5 @@ const app = (
   </Provider>
 );
 
-ReactDOM.render(app, document.getElementById("root"));
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
