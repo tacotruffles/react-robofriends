a41593c3df01430782e3adac70feae7d9cd7ef15
John Dawes <jdawes@bluebitmedia.com>
2018-08-05 15:58:20 -0600

    Snapshot test for Card.js

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -2,10 +2,10 @@ import React from 'react';
 // shallow = silbings rendered only
 // mount = renders with DOM
 // render = renders to static HTML
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import Card from './Card';
 
 // Shallow render to test card component only - no children
 it('expect to render card component', () => {
-  expect(shallow(<Card />).length).toEqual(1);
+  expect(shallow(<Card />)).toMatchSnapshot();
 });
